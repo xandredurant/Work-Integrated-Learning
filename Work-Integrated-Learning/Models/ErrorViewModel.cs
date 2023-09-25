87f9f1f1c50dd5f0228bc54ea1e95d4d32380c40
XANDRE DURANT <20230681@ctucareer.co.za>
2023-09-25 16:41:50 +0200

    Add project files.

---
+++ Work-Integrated-Learning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Work_Integrated_Learning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
