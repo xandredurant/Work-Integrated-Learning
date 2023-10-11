3d11c462ef6d8da2d032fb29844a36839da4d4a9
XANDRE DURANT <20230681@ctucareer.co.za>
2023-10-11 14:32:08 +0200

    Add Project

---
+++ WIL Project/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WIL_Project.Models;
+
+namespace WIL_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Events()
+        {
+            return View();
+        }
+
+        public IActionResult Schedule()
+        {
+            return View();
+        }
+        public IActionResult Program()
+        {
+            return View();
+        }
+
+        public IActionResult survey()
+        {
+            return View();
+        }
+
+        public IActionResult contact()
+        {
+            return View();
+        }
+
+        public IActionResult speakers()
+        {
+            return View();
+        }
+        public IActionResult Voucher()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WIL Project/wwwroot/js/voucher.js
@@ -0,0 +1,28 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const vouchers = document.querySelectorAll('.voucher');
+    const voucherDetails = document.querySelectorAll('.voucher-details');
+
+    vouchers.forEach((voucher, index) => {
+        voucher.addEventListener('click', () => {
+            // Toggle the display of voucher details
+            voucherDetails[index].classList.toggle('show-details');
+
+            // Check if any other voucher details are open
+            const otherOpenDetails = Array.from(voucherDetails).filter((detail, i) => i !== index && detail.classList.contains('show-details'));
+
+            if (otherOpenDetails.length > 0) {
+                // If other voucher details are open, arrange them horizontally
+                otherOpenDetails.forEach((detail) => {
+                    detail.classList.remove('show-details');
+                    detail.style.left = '0';
+                });
+                voucherDetails[index].style.left = '50%';
+            } else {
+                // If no other voucher details are open, reset the layout
+                voucherDetails.forEach((detail) => {
+                    detail.style.left = '0';
+                });
+            }
+        });
+    });
+});
