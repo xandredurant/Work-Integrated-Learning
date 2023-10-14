40bef30bf83dc86ebbd26875e3ddb3b3a7c0df7a
Liam Swanepoel <Liamswanepoel03@gmail.com>
2023-10-14 18:34:03 +0200

    Create DiscountVoucher.cs

---
+++ WIL Project/Models/DiscountVoucher.cs
@@ -0,0 +1,16 @@
+﻿namespace WIL_Project.Models
+{
+    public class DiscountVoucher
+    {
+        public string Code { get; set; }
+        public string DiscountType { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+        public int LimitPerUser { get; set; }
+        public int TimesUsed { get; set; }
+        public string ApplicableEvents { get; set; }
+
+        // Navigation property for redemptions related to this discount
+        public List<DiscountVoucherRedemption> Redemptions { get; set; }
+    }
+}
