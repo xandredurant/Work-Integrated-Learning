d694bad5780d472ba9ecf4cc5579f8be64d1f1c0
Janderik Eloff <janderik03@gmail.com>
2023-10-14 20:31:52 +0200

    LoginController

---
+++ WIL Project/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WIL_Project.Areas.Identity.Data;
+using WIL_Project.Models;
+
+namespace WIL_Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<SampleUser> _signInManager;
+
+        public LoginController(SignInManager<SampleUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    // Successfully logged in
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+            return View(model);
+        }
+
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
+
