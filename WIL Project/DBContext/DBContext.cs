f916bf8730d227be54ccc1960fc40464dbb5b7cb
XANDRE DURANT <20230681@ctucareer.co.za>
2023-10-14 02:51:41 +0200

    Create DBContext. Still need to create the models.

---
+++ WIL Project/DBContext/DBContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace WIL_Project.DBContext
+{
+    public class DBContext
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EventInformation> EventInformation { get; set; }
+        public DbSet<SessionInformation> SessionInformation { get; set; }
+        public DbSet<SpeakerInformation> SpeakerInformation { get; set; }
+        public DbSet<UserInformation> UserInformation { get; set; }
+        public DbSet<DiscountAndVoucher> DiscountAndVoucher { get; set; }
+        public DbSet<DiscountAndVoucherRedemption> DiscountAndVoucherRedemption { get; set; }
+        public DbSet<ReviewAndRating> ReviewAndRating { get; set; }
+        public DbSet<Survey> Survey { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Define relationships and constraints here if needed
+            modelBuilder.Entity<SessionInformation>()
+                .HasOne(s => s.EventInformation)
+                .WithMany(e => e.Sessions)
+                .HasForeignKey(s => s.EventID);
+
+            // Add other relationships and constraints as needed
+        }
+    }
+}
