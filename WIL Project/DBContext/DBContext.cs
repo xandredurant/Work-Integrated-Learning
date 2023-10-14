6020f3b4290e3ef1190adf39fbbd197ee5b45b6d
Liam Swanepoel <Liamswanepoel03@gmail.com>
2023-10-14 16:30:53 +0200

    Update DBContext.cs

--- WIL Project/DBContext/DBContext.cs
+++ WIL Project/DBContext/DBContext.cs
@@ -12,10 +12,10 @@ namespace WIL_Project.DBContext
         public DbSet<EventInformation> EventInformation { get; set; }
         public DbSet<SessionInformation> SessionInformation { get; set; }
         public DbSet<SpeakerInformation> SpeakerInformation { get; set; }
-        public DbSet<UserInformation> UserInformation { get; set; }
-        public DbSet<DiscountAndVoucher> DiscountAndVoucher { get; set; }
-        public DbSet<DiscountAndVoucherRedemption> DiscountAndVoucherRedemption { get; set; }
-        public DbSet<ReviewAndRating> ReviewAndRating { get; set; }
+        public DbSet<UserInfo> UserInfo { get; set; }
+        public DbSet<DiscountVoucher> DiscountVoucher { get; set; }
+        public DbSet<DiscountVoucherRedemption> DiscountVoucherRedemption { get; set; }
+        public DbSet<ReviewRating> ReviewRating { get; set; }
         public DbSet<Survey> Survey { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
