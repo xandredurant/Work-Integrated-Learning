757206d03cb7f53935570234dab2d31046876fa7
XANDRE DURANT <20230681@ctucareer.co.za>
2023-10-14 18:44:05 +0200

    Create separate controllers for events and session (program -> session)

---
+++ WIL Project/Controllers/SessionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WIL_Project.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
