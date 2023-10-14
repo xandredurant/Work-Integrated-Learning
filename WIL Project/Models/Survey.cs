e44ede78972e2147edfde2a883593dfa80e14792
Liam Swanepoel <Liamswanepoel03@gmail.com>
2023-10-14 16:49:19 +0200

    Update Survey.cs

--- WIL Project/Models/Survey.cs
+++ WIL Project/Models/Survey.cs
@@ -12,6 +12,6 @@
         // Navigation properties for related event, session, and user
         public EventInformation EventInformation { get; set; }
         public SessionInformation SessionInformation { get; set; }
-        public UserInformation UserInformation { get; set; }
+        public UserInfo UserInfo { get; set; }
     }
 }
