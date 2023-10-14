be5e98c12f1239bfcaed01b019daa790e341605b
Janderik Eloff <janderik03@gmail.com>
2023-10-14 20:25:57 +0200

    LoginModel

---
+++ WIL Project/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WIL_Project.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
