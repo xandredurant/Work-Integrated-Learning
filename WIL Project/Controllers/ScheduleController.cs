ee12209c7d37a5e7dd0c6d0b89dc16fadeadae09
Janderik Eloff <janderik03@gmail.com>
2023-10-17 13:08:13 +0200

    Controller Updated

--- WIL Project/Controllers/ScheduleController.cs
+++ WIL Project/Controllers/ScheduleController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WIL_Project.DBContext;
+using WIL_Project.Models;
 
 namespace WIL_Project.Controllers
 {
     public class ScheduleController : Controller
     {
+        private readonly MyDbContext _context;
+
+        public ScheduleController(MyDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var events = _context.EventInformation.ToList(); // Fetch all events from the database
+            return View(events);
         }
     }
 }
