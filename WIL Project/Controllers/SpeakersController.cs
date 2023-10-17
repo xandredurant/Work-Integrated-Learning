35317bed8a54034c9e0199638ab298cd5d4a8e87
Janderik Eloff <janderik03@gmail.com>
2023-10-18 00:11:40 +0200

    Speakers Folder Update

---
+++ WIL Project/Controllers/SpeakersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WIL_Project.Controllers
+{
+    public class SpeakersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
