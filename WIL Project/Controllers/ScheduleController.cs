a8e57d542fb2c59c5d7e1cf88e7b3554e7371717
Janderik Eloff <janderik03@gmail.com>
2023-10-17 13:04:17 +0200

    Controller update

--- WIL Project/Controllers/ScheduleController.cs
+++ WIL Project/Controllers/ScheduleController.cs
@@ -1,42 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using WIL_Project.DBContext;
-using WIL_Project.Models;
-using Serilog; // You need to import the Serilog namespace
 
-public class ScheduleController : Controller
+namespace WIL_Project.Controllers
 {
-    private readonly MyDbContext _context;
-
-    public ScheduleController(MyDbContext context)
+    public class ScheduleController : Controller
     {
-        _context = context;
-    }
-
-    public async Task<IActionResult> Index()
-    {
-        try
+        public IActionResult Index()
         {
-            var events = await _context.EventInformation.ToListAsync();
-
-            if (events == null)
-            {
-                // Log a message using Serilog
-                Log.Error("Failed to retrieve events from the database.");
-                // You can customize the logging as needed
-            }
-
-            return View(events);
-        }
-        catch (Exception ex)
-        {
-            // Log the exception using Serilog or another logging library
-            Log.Error(ex, "An error occurred while fetching events from the database.");
-            // Handle the exception, perhaps display an error page
-            return View("Error");
+            return View();
         }
     }
 }
