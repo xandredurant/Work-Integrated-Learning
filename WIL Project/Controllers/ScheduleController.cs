f01585eb94690c251144c7983aa56f75edebf2fd
Janderik Eloff <janderik03@gmail.com>
2023-10-17 13:36:55 +0200

    123

--- WIL Project/Controllers/ScheduleController.cs
+++ WIL Project/Controllers/ScheduleController.cs
@@ -1,29 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic; // Make sure to import this namespace
+using System.Linq;
+using WIL_Project.DBContext;
 using WIL_Project.Models;
 
 namespace WIL_Project.Controllers
 {
-    public class EventController : Controller
+    public class ScheduleController : Controller
     {
+        private readonly MyDbContext _context;
+
+        public ScheduleController(MyDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            // Assuming you have a list of events that you want to display
-            var events = new List<EventInformation>
-            {
-                // Populate this list with EventInformation objects retrieved from the database
-                new EventInformation
-                {
-                    // Fill in properties for the event
-                    EventName = "Sample Event 1",
-                    EventDate = DateTime.Now,
-                    EventType = "Seminar",
-                    EventDescription = "This is a sample event description."
-                },
-                // Add more events as needed
-            };
-
-            return View(events); // Pass the list of events to the view
+            var events = _context.EventInformation.Take(9).ToList(); // Fetch the first 9 events from the database
+            return View(events);
         }
     }
 }
