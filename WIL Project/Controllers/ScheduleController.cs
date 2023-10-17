aed630ae079674e26bd63e01f6d81658085e5223
Janderik Eloff <janderik03@gmail.com>
2023-10-17 12:47:52 +0200

    ScheduleController

---
+++ WIL Project/Controllers/ScheduleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WIL_Project.DBContext;
+using WIL_Project.Models;
+
+public class ScheduleController : Controller
+{
+    private readonly MyDbContext _context;
+
+    public ScheduleController(MyDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var events = await _context.EventInformation.ToListAsync();
+        return View(events);
+    }
+}
