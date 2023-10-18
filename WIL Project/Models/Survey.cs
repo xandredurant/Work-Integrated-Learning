4d8c98405e574b67b90fb5dcce3d6999f879c5c5
Janderik Eloff <janderik03@gmail.com>
2023-10-18 14:40:36 +0200

    Survey Model Update

--- WIL Project/Models/Survey.cs
+++ WIL Project/Models/Survey.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WIL_Project.Models
 {
+    [Table("Survey")]
     public class Survey
     {
         public int SurveyID { get; set; }
