08b1662299dd0dc3b89954b5c109bf988e02c49d
Lebogang Tyosho <lebogang15@icloud.com>
2023-10-18 14:00:38 +0200

    Changed Model

--- WIL Project/Models/DiscountVoucher.cs
+++ WIL Project/Models/DiscountVoucher.cs
@@ -15,7 +15,7 @@ namespace WIL_Project.Models
         public DateTime ValidTo { get; set; }
         public int LimitPerUser { get; set; }
         public int TimesUsed { get; set; }
-        public string ApplicableEvents { get; set; }
+        public string EventID { get; set; }
 
         // Navigation property for redemptions related to this discount
         public List<DiscountVoucherRedemption> Redemptions { get; set; }
